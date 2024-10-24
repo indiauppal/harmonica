dec25cf0e78554415c6f06e27eb7a1acaca8bc40
indiauppal <indiakuppal@gmail.com>
2024-10-24 18:34:02 +0100

    Create fit function for magnetic equivalent sources

--- harmonica/_equivalent_sources/magnetic.py
+++ harmonica/_equivalent_sources/magnetic.py
@@ -11,6 +11,7 @@ import numpy as np
 import verde as vd
 import verde.base as vdb
 import choclo
+from .. import magnetic_angles_to_vec
 from numba import jit
 from sklearn.utils.validation import check_is_fitted
 
@@ -54,3 +55,54 @@ class EquivalentSourcesMagnetic():
         bn=None,
         bu=None,
     ):
+        coordinates, data, weights, inclination, declination = tfa
+        coordinates, data, weights = vdb.check_fit_input(coordinates, data, weights)
+        # Capture the data region to use as a default when gridding.
+        self.region_ = vd.get_region(coordinates[:2])
+        coordinates = vdb.n_1d_arrays(coordinates, 3)
+        if self.dipole_coordinates is None:
+            self.dipole_coordinates_ = tuple(
+                p.astype(self.dtype) for p in self._build_points(coordinates)
+            )
+        else:
+            self.depth_ = None  # set depth_ to None so we don't leave it unset
+            self.dipole_coordinates_ = tuple(
+                p.astype(self.dtype) for p in vdb.n_1d_arrays(self.dipole_coordinates, 3)
+            )
+        dipole_moment_direction = magnetic_angles_to_vec(
+            1, self.dipole_inclination, self.dipole_declination,
+        )
+        field_direction = magnetic_angles_to_vec(1, inclination, declination)
+        jacobian = self.jacobian_tfa(
+            coordinates, self.dipole_coordinates_, dipole_moment_direction, field_direction,
+        )
+        moment_amplitude = vdb.least_squares(jacobian, data, weights, self.damping)
+        self.dipole_moments_ = magnetic_angles_to_vec(
+            moment_amplitude, self.dipole_inclination, self.dipole_declination,
+        )
+        return self
+    
+    def _build_dipoles(self, coordinates):
+        """
+        """
+        if self.block_size is not None:
+            reducer = vd.BlockReduce(
+                spacing=self.block_size, reduction=np.median, drop_coords=False
+            )
+            # Must pass a dummy data array to BlockReduce.filter(), we choose an
+            # array full of zeros. We will ignore the returned reduced dummy array.
+            coordinates, _ = reducer.filter(coordinates, np.zeros_like(coordinates[0]))
+        if self.depth == "default":
+            self.depth_ = 4.5 * np.mean(vd.median_distance(coordinates, k_nearest=1))
+        else:
+            self.depth_ = self.depth
+        return (
+            coordinates[0],
+            coordinates[1],
+            coordinates[2] - self.depth_,
+        )
+    
+    def jacobian(
+        self, coordinates, dipole_coordinates, dipole_moment_direction, field_direction,
+    ):
+        
