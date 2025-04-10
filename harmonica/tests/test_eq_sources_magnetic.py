74b26f940bd456fdb981218e7d02042cfc69ce3f
indiauppal <indiakuppal@gmail.com>
2025-04-10 18:18:08 +0100

    Create tests for predicting on scatter coords

--- harmonica/tests/test_eq_sources_magnetic.py
+++ harmonica/tests/test_eq_sources_magnetic.py
@@ -12,26 +12,48 @@ import verde as vd
 import harmonica as hm
 
 
-# To do
-# scattered coordinates, same scattered synthetic model
-# Fit scatter grid, test on regular grid
+region = [-3e3, 2e3, -4e3, 5e3]
+shape=[30, 30]
+inc, dec = 75, 60
+source_coords = vd.grid_coordinates(region=region, shape=shape, extra_coords=-800)
+checker = vd.synthetic.CheckerBoard(1e9, region, w_east=2e3, w_north=4e3)
+source_magnitudes = checker.predict(source_coords)
+source_dipole_moments = hm.magnetic_angles_to_vec(source_magnitudes, inc, dec)
+grid_coords = vd.grid_coordinates(region=region, shape=shape, extra_coords=500)
+scatter_coords = vd.scatter_points(region=region, size=(shape[0]*shape[1]), extra_coords=500)
 
 def test_on_regular_grid():
-    region = [-3e3, 2e3, -4e3, 5e3]
-    shape=[30, 30]
-    inc, dec = 75, 60
-    source_coords = vd.grid_coordinates(region=region, shape=shape, extra_coords=-800)
-    checker = vd.synthetic.CheckerBoard(1e9, region, w_east=2e3, w_north=4e3)
-    source_magnitudes = checker.predict(source_coords)
-    source_dipole_moments = hm.magnetic_angles_to_vec(source_magnitudes, inc, dec)
-
-    data_coords = vd.grid_coordinates(region=region, shape=shape, extra_coords=500)
-    be, bn, bu = hm.dipole_magnetic(data_coords, source_coords, source_dipole_moments, field='b')
+    be, bn, bu = hm.dipole_magnetic(grid_coords, source_coords, source_dipole_moments, field='b')
     tfa = hm.total_field_anomaly([be, bn, bu], inc, dec)
 
     eqs = hm.EquivalentSourcesTotalFieldAnomaly()
-    eqs.fit(data_coords, tfa, inc, dec)
-    tfa_predict = eqs.predict(data_coords, inc, dec)
+    eqs.fit(grid_coords, tfa, inc, dec)
+    tfa_predict = eqs.predict(grid_coords, inc, dec)
     atol = vd.maxabs(tfa) / 1e5
 
     np.testing.assert_allclose(tfa, tfa_predict, atol=atol)
+
+def test_on_scatter_grid():
+    be, bn, bu = hm.dipole_magnetic(scatter_coords, source_coords, source_dipole_moments, field='b')
+    tfa = hm.total_field_anomaly([be, bn, bu], inc, dec)
+
+    eqs = hm.EquivalentSourcesTotalFieldAnomaly()
+    eqs.fit(scatter_coords, tfa, inc, dec)
+    tfa_predict = eqs.predict(scatter_coords, inc, dec)
+    atol = vd.maxabs(tfa) / 1e3
+
+    np.testing.assert_allclose(tfa, tfa_predict, atol=atol)
+
+def test_fit_grid_predict_scatter():
+    be, bn, bu = hm.dipole_magnetic(grid_coords, source_coords, source_dipole_moments, field='b')
+    tfa = hm.total_field_anomaly([be, bn, bu], inc, dec)
+    true_be, true_bn, true_bu = hm.dipole_magnetic(scatter_coords, source_coords, source_dipole_moments, field='b')
+    true_tfa = hm.total_field_anomaly([true_be, true_bn, true_bu], inc, dec)
+    
+    eqs = hm.EquivalentSourcesTotalFieldAnomaly()
+    eqs.fit(grid_coords, tfa, inc, dec)
+    tfa_predict = eqs.predict(scatter_coords, inc, dec)
+    rmse = np.sqrt(np.nanmean((true_tfa - tfa_predict)**2))
+    tol = vd.maxabs(tfa)/1e3
+    
+    assert rmse <= tol
